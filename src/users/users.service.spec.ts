90d43c5e5e5d27dc402105075c3619b3ff9a5331
Usman Afzal <109211422+usmannafzal@users.noreply.github.com>
2024-12-31 16:06:07 +0500

    Tests added and dotenv package added

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,118 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { UsersController } from './users.controller';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
-describe('UsersService', () => {
-  let service: UsersService;
+describe('UsersController', () => {
+  let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    // Create fake/mock services
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'test@example.com',
+          password: '12345',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: '12345' } as User]);
+      },
+      remove: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'test@example.com',
+          password: '12345',
+        } as User);
+      },
+      update: (id: number, attrs: Partial<User>) => {
+        return Promise.resolve({
+          id,
+          email: 'test@example.com',
+          password: '12345',
+          ...attrs,
+        } as User);
+      },
+    };
+
+    fakeAuthService = {
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+      signup: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+    };
+
+    // Create testing module
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: CurrentUserInterceptor,
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+    expect(user.id).toEqual(1);
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('test@example.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('test@example.com');
+  });
+
+  it('signin updates session and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signIn(
+      { email: 'test@example.com', password: '12345' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  });
+
+  it('signup creates a new user and updates session', async () => {
+    const session = { userId: -10 };
+    const user = await controller.createUser(
+      { email: 'test@example.com', password: '12345' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  });
+
+  it('signout removes the userId from the session', () => {
+    const session = { userId: 1 };
+    controller.signOut(session);
+    expect(session.userId).toBeNull();
   });
 });
